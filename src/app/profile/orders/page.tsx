0458faed660f7987bf140d6a69fb02d43db537f0
thatAwesomeKK <32800130+thatAwesomeKK@users.noreply.github.com>
2023-09-22 01:05:56 +0530

    minimal responsive

--- src/app/profile/orders/page.tsx
+++ src/app/profile/orders/page.tsx
@@ -20,9 +20,9 @@ const Orders = async () => {
     const orders: Service[] = await fetchProfileServiceRequest(accessToken)
 
     return (
-        <main className="max-w-7xl mx-auto py-10">
+        <main className="lg:max-w-7xl mx-auto py-10">
             <h1 className={`${permanentMarker.className} text-gray-600 text-8xl font-bold text-center mb-16 underline`}>Orders</h1>
-            <section className='grid grid-cols-3 gap-10'>
+            <section className='flex flex-col justify-center items-center md:grid gap-10 md:grid-cols-2 lg:grid-cols-3'>
                 <OrdersContainer orders={orders} />
             </section>
         </main>
--- src/components/Home/Services/AvailableServices.tsx
+++ src/components/Home/Services/AvailableServices.tsx
@@ -15,7 +15,13 @@ const AvailableServices = async () => {
     return (
         <section className='my-10 mx-20'>
             <h1 className={`${libreBaskerville.className} text-gray-600 text-8xl font-bold text-center mb-16`}>Available Services</h1>
-            {services.length > 0 ? <div className='grid grid-cols-4 gap-10'>
+            {services.length > 0 ? <div className='
+            grid grid-cols-1 w-full max-w-[50rem] gap-10 
+            sm:grid-cols-2 sm:max-w-[70rem] 
+            md:grid-cols-2 md:max-w-[100rem] 
+            lg:grid-cols-3 lg:gap-15 
+            xl:grid-cols-4 xl:gap-20 
+            2xl:max-w-none 3xl:grid-cols-5'>
                 {services.map((service, i: number) => (
                     <ServiceCard key={i} service={service} />
                 ))}
